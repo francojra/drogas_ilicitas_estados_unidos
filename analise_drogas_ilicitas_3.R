e94446f49aec6a5236f4d5b48f457e1e869b3e8a
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-01-09 22:44:27 -0300

    Script

--- analise_drogas_ilicitas_3.R
+++ analise_drogas_ilicitas_3.R
@@ -82,8 +82,10 @@ g4
 library(gridExtra) # Pacote para unir os gráficos em uma janela
 library(grid) # Pacote que permite usar o argumento de título textGorb
 
+tg <- textGrob('Estados Unidos', gp = gpar(fontsize = 13, fontface = 'bold'))
+
 plot_eua <- grid.arrange(g1, g2, g3, g4, ncol = 2,  # ncol se refere a número de colunas
-                    top = textGrob("Estados Unidos", gp = gpar(fontsize = 22, col = "#4575b4")))
+                    top = tg)
 
 # Salvar gráfico ---------------------------------------------------------------------------------------------------------------------------
 
